ce2b51d4c65cac09e99d8007286908e34107748d
bhima2001 <shankardb369@gmail.com>
2025-07-25 17:18:02 +0000

    made changed in app.js and server.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import usersRoutes from './routes/users.routes.js';
+import parkingLotsRoutes from './routes/parkingLots.routes.js';
+import reservationsRoutes from './routes/reservations.routes.js';
+import parkingSpotsRoutes from './routes/parkingSpots.routes.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { StatusCodes } from 'http-status-codes';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use('/api', usersRoutes);
+app.use('/api', parkingLotsRoutes);
+app.use('/api', reservationsRoutes);
+app.use('/api', parkingSpotsRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Route not found');
+    error.statusCode = StatusCodes.NOT_FOUND;
+    next(error);
 });
 
+app.use(errorHandler);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,5 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
-    console.log(port, host);
     console.log(`Server is running on http://${host}:${port}`);
 });
